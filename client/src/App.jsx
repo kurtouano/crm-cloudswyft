ece97c32cb343fe070acfdca53e6623dd888b583
kurtouano <kurt0216@gmail.com>
2025-02-17 15:54:01 +0800

    Add all pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import Salesflow from './pages/Salesflow';
+import Accounts from './pages/Accounts';
+import Employees from './pages/Employees';
+import Communications from './pages/Communications';
 
 export default function App() {
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/sales-flow" element={<Salesflow />} />
+      <Route path="/accounts" element={<Accounts />} />
+      <Route path="/employees" element={<Employees />} />
+      <Route path="/communications" element={<Communications />} />
     </Routes>
   </BrowserRouter>;
 }
