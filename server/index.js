81814a4bbab6b3369d4bf80d86c4f8cbf3dbc6c3
kurtouano <kurt0216@gmail.com>
2025-02-17 15:01:14 +0800

    Tailwind Installation

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.listen(3001, () => {
+  console.log('Server is running on http://localhost:3001');
 });
 
 app.get('/', (req, res) => {
