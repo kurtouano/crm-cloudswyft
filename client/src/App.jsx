8c18b7af91fe4ff14557363f95482bbfbd8cb490
nιcĸ <nhickandreifabian@gmail.com>
2025-02-17 18:47:22 +0800

    create navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Sidenav from './components/sidenav/Sidenav'; // Import Sidenav
 import Dashboard from './pages/Dashboard';
 import Salesflow from './pages/Salesflow';
 import Accounts from './pages/Accounts';
@@ -6,13 +7,20 @@ import Employees from './pages/Employees';
 import Communications from './pages/Communications';
 
 export default function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/sales-flow" element={<Salesflow />} />
-      <Route path="/accounts" element={<Accounts />} />
-      <Route path="/employees" element={<Employees />} />
-      <Route path="/communications" element={<Communications />} />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <div className="flex">
+        <Sidenav /> {/* Include Sidenav */}
+        <div className="ml-64 p-4 w-full"> {/* Add margin to make space for sidenav */}
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/sales-flow" element={<Salesflow />} />
+            <Route path="/accounts" element={<Accounts />} />
+            <Route path="/employees" element={<Employees />} />
+            <Route path="/communications" element={<Communications />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
