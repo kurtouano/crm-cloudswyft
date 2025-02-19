bd987210d13180cd48d56a737253e71cf8f4dc12
nιcĸ <nhickandreifabian@gmail.com>
2025-02-19 13:28:39 +0800

    add login input validation

--- client/src/components/sidenav/Sidenav.jsx
+++ client/src/components/sidenav/Sidenav.jsx
@@ -27,7 +27,6 @@ const Sidenav = () => {
 
     const navigate = useNavigate();
 
-    // 🔴 Logout Function
     const handleLogout = () => {
         localStorage.removeItem("token"); // Remove JWT Token
         localStorage.removeItem("role");  // Remove User Role
