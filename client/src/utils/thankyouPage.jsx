5e32296e71be240d66629f284351c1397fc107a8
kurtouano <kurt0216@gmail.com>
2025-04-17 15:19:48 +0800

    - Added Thank you page upon customer support feedback in email. - Minor styling adjustments on sales team page and dashboard

---
+++ client/src/utils/thankyouPage.jsx
@@ -0,0 +1,20 @@
+import "./thankyouPage.css";
+
+export default function ThankYouPage() {
+  return (
+    <>
+    <div className="thank-you-page-styles">
+      <div className="thank-you-container">
+        <div className="thank-you-card">
+        <h1 className="company-name">CloudSwyft</h1>
+          <div className="thank-you-content">
+            <h2>Thank You!</h2>
+            <p>We appreciate your feedback.😊</p>
+            <p className="follow-up">Your response has been recorded.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
