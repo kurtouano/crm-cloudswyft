9040177df8b2c7a4e662e157632aac06191f6b2b
kurtouano <kurt0216@gmail.com>
2025-04-18 23:29:05 +0800

    Aadded assetsInclude in vite.config for mp3 error in vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     tailwindcss(),
   ],
   base: '/',
+  assetsInclude: ['**/*.mp3'], 
   server: {
     host: '0.0.0.0',  // Makes the app accessible from your local network
     port: 3000,        // Change to your desired port
